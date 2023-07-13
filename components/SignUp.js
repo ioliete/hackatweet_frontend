830dfb1f4c9d63f73a301960a6727a348be1ae72
Ines <ines.oliete@gmail.com>
2023-07-13 15:49:45 +0200

    màj pages/_app.js

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,51 +1,113 @@
-import { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Modal } from 'antd';
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Modal } from "antd";
 
-import styles from '../styles/Login.module.css';
+import styles from "../styles/Login.module.css";
 
 function SignUp() {
-    const [isModalVisible, setIsModalVisible] = useState(false);
-	const [signUpFirstname, setSignUpFirstname] = useState('');
-	const [signUpUsername, setSignUpUsername] = useState('');
-	const [signUpPassword, setSignUpPassword] = useState('');
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
 
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [signUpFirstname, setSignUpFirstname] = useState("");
+  const [signUpUsername, setSignUpUsername] = useState("");
+  const [signUpPassword, setSignUpPassword] = useState("");
 
-const handleRegister = () => {
-    fetch('http://localhost:3000/users/signup', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
+  const handleRegister = () => {
+    fetch("http://localhost:3000/users/signup", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ firstname: signUpFirstname, username: signUpUsername, password: signUpPassword }),
-    }).then(response => response.json())
-      .then(data => {
+    })
+      .then((response) => response.json())
+      .then((data) => {
         if (data.result) {
           dispatch(login(signUpUsername));
-          setSignUpFirstname('');
-          setSignUpUsername('');
-          setSignUpPassword('');
+          setSignUpFirstname("");
+          setSignUpUsername("");
+          setSignUpPassword("");
         }
       });
   };
 
   const showModal = () => {
     setIsModalVisible(!isModalVisible);
-};
+  };
 
-let modalContent;
-if (!user.isConnected) {
+  let modalContent;
+  if (!user.isConnected) {
     modalContent = (
-        <div className={styles.registerContainer}>
-            <div className={styles.registerSection}>
-                <p>Sign-up</p>
-                <input type="text" placeholder="Firstname" id="signUpFirstname" onChange={(e) => setSignUpFirstname(e.target.value)} value={signUpFirstname} />
-                <input type="text" placeholder="Username" id="signUpUsername" onChange={(e) => setSignUpUsername(e.target.value)} value={signUpUsername} />
-                <input type="password" placeholder="Password" id="signUpPassword" onChange={(e) => setSignUpPassword(e.target.value)} value={signUpPassword} />
-                <button id="register" onClick={() => handleRegister()}>Sign up</button>
-            </div>
-
+      <div className={styles.registerContainer}>
+        <div className={styles.registerSection}>
+          <p>Sign-up</p>
+          <input
+            type="text"
+            placeholder="Firstname"
+            id="signUpFirstname"
+            onChange={(e) => setSignUpFirstname(e.target.value)}
+            value={signUpFirstname}
+          />
+          <input
+            type="text"
+            placeholder="Username"
+            id="signUpUsername"
+            onChange={(e) => setSignUpUsername(e.target.value)}
+            value={signUpUsername}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="signUpPassword"
+            onChange={(e) => setSignUpPassword(e.target.value)}
+            value={signUpPassword}
+          />
+          <button id="register" onClick={() => handleRegister()}>
+            Sign up
+          </button>
         </div>
+      </div>
     );
-}
+  }
+
+  let userSection;
+  if (user.token) {
+    userSection = (
+      <>
+        <div className={styles.logoutSection}>
+          <p>Welcome {user.username} / </p>
+          <button onClick={() => handleLogout()}>Logout</button>
+        </div>
+        <div className={styles.headerIcons}>
+          <FontAwesomeIcon onClick={handleShowAllArticles} className={styles.userSection} icon={faEye} />
+        </div>
+      </>
+    );
+  } else {
+    if (isModalVisible) {
+      userSection = (
+        <>
+          <div className={styles.headerIcons}>
+            <FontAwesomeIcon onClick={showModal} className={styles.userSection} icon={faXmark} />
+          </div>
+
+          <div className={styles.headerIcons}>
+            <FontAwesomeIcon onClick={handleShowAllArticles} className={styles.userSection} icon={faEye} />
+          </div>
+        </>
+      );
+    } else {
+      userSection = (
+        <>
+          <div className={styles.headerIcons}>
+            <FontAwesomeIcon onClick={showModal} className={styles.userSection} icon={faUser} />
+          </div>
+          <div className={styles.headerIcons}>
+            <FontAwesomeIcon onClick={handleShowAllArticles} className={styles.userSection} icon={faEye} />
+          </div>
+        </>
+      );
+    }
+  }
 
   return (
     <header className={styles.header}>
@@ -54,14 +116,21 @@ if (!user.isConnected) {
         {userSection}
       </div>
 
-      {isModalVisible && <div id="react-modals">
-        <Modal getContainer="#react-modals" className={styles.modal} visible={isModalVisible} closable={false} footer={null}>
-          {modalContent}
-        </Modal>
-      </div>}
-    </header >
+      {isModalVisible && (
+        <div id="react-modals">
+          <Modal
+            getContainer="#react-modals"
+            className={styles.modal}
+            visible={isModalVisible}
+            closable={false}
+            footer={null}
+          >
+            {modalContent}
+          </Modal>
+        </div>
+      )}
+    </header>
   );
-
 }
 
 export default SignUp;
